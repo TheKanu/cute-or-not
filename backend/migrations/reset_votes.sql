365cb1d266ea66fd027cd7a327d460bdbe279082
theKanu <keanu.amann@outlook.com>
2025-05-27 12:11:06 +0200

    fixes

--- backend/migrations/reset_votes.sql
+++ backend/migrations/reset_votes.sql
@@ -1,4 +1,4 @@
-BEGIN;
+/* BEGIN;
 
 -- 1) Delete all vote records and reset the votes table’s serial/identity
 TRUNCATE TABLE votes RESTART IDENTITY;
@@ -9,3 +9,5 @@ SET cute_score  = 0,
     total_votes = 0;
 
 COMMIT;
+
+*/
